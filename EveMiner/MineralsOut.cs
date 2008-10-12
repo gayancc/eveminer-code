76d445cf6cb3e2651e0017a06070354b70c0fd87
s1eepwa1ker <s1eepwa1ker@ae06a2a2-a94a-0410-8aa0-debe1e830465>
2008-10-12 07:35:50 +0000

    + Комментарии + Энкапсуляция полей

--- EveMiner/MineralsOut.cs
+++ EveMiner/MineralsOut.cs
@@ -1,26 +1,128 @@
 namespace EveMiner
 {
+	/// <summary>
+	/// Выход минералов для руды
+	/// </summary>
 	public class MineralsOut
 	{
-		public int Tritanium;
-		public int Pyerite;
-		public int Mexallon;
-		public int Isogen;
-		public int Nocxium;
-		public int Zydrine;
-		public int Megacyte;
-		public int Morphite;
+		/// <summary>
+		///  Выход трита с одной единицы руды
+		/// </summary>
+		private readonly int tritanium;
+		/// <summary>
+		///  Выход пирита с одной единицы руды
+		/// </summary>
+		private readonly int pyerite;
+		/// <summary>
+		///  Выход мексаллона с одной единицы руды
+		/// </summary>
+		private readonly int mexallon;
+		/// <summary>
+		///  Выход изогена с одной единицы руды
+		/// </summary>
+		private readonly int isogen;
+		/// <summary>
+		///  Выход ноксы с одной единицы руды
+		/// </summary>
+		private readonly int nocxium;
+		/// <summary>
+		///  Выход зидры с одной единицы руды
+		/// </summary>
+		private readonly int zydrine;
+		/// <summary>
+		///  Выход меги с одной единицы руды
+		/// </summary>
+		private readonly int megacyte;
+		/// <summary>
+		///  Выход морфита с одной единицы руды
+		/// </summary>
+		private readonly int morphite;
 
+		/// <summary>
+		/// Конструктор по значениям выхода минералов
+		/// </summary>
+		/// <param name="tritanium">выход трита</param>
+		/// <param name="pyerite">выход пирита</param>
+		/// <param name="mexallon">выход мексаллона</param>
+		/// <param name="isogen">выход изогена</param>
+		/// <param name="nocxium">выход ноксы</param>
+		/// <param name="zydrine">выход зидры</param>
+		/// <param name="megacyte">выход мегацита</param>
+		/// <param name="morphite">выход морфита</param>
 		public MineralsOut(int tritanium, int pyerite, int mexallon, int isogen, int nocxium, int zydrine, int megacyte, int morphite)
 		{
-			Tritanium = tritanium;
-			Pyerite = pyerite;
-			Mexallon = mexallon;
-			Isogen = isogen;
-			Nocxium = nocxium;
-			Zydrine = zydrine;
-			Megacyte = megacyte;
-			Morphite = morphite;
+			this.tritanium = tritanium;
+			this.pyerite = pyerite;
+			this.mexallon = mexallon;
+			this.isogen = isogen;
+			this.nocxium = nocxium;
+			this.zydrine = zydrine;
+			this.megacyte = megacyte;
+			this.morphite = morphite;
+		}
+
+		/// <summary>
+		///  Выход трита с одной единицы руды
+		/// </summary>
+		public int Tritanium
+		{
+			get { return tritanium; }
+		}
+
+		/// <summary>
+		///  Выход пирита с одной единицы руды
+		/// </summary>
+		public int Pyerite
+		{
+			get { return pyerite; }
+		}
+
+		/// <summary>
+		///  Выход мексаллона с одной единицы руды
+		/// </summary>
+		public int Mexallon
+		{
+			get { return mexallon; }
+		}
+
+		/// <summary>
+		///  Выход изогена с одной единицы руды
+		/// </summary>
+		public int Isogen
+		{
+			get { return isogen; }
+		}
+
+		/// <summary>
+		///  Выход ноксы с одной единицы руды
+		/// </summary>
+		public int Nocxium
+		{
+			get { return nocxium; }
+		}
+
+		/// <summary>
+		///  Выход зидры с одной единицы руды
+		/// </summary>
+		public int Zydrine
+		{
+			get { return zydrine; }
+		}
+
+		/// <summary>
+		///  Выход меги с одной единицы руды
+		/// </summary>
+		public int Megacyte
+		{
+			get { return megacyte; }
+		}
+
+		/// <summary>
+		///  Выход морфита с одной единицы руды
+		/// </summary>
+		public int Morphite
+		{
+			get { return morphite; }
 		}
 	}
 }
